54673e0804cc21009d7566c07d66440e4778ea37
Pranav <95164530+thefakepranav@users.noreply.github.com>
2023-04-09 16:52:01 +0530

    minor changes in getShopsById API

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -4,25 +4,30 @@ import { Item } from "../models/order.model.js";
 import mongoose from "mongoose";
 
 export const getShopById = async (req, res) => {
-  const id = req.params.id;
-  const shopDoc = await Shop.findOne({ _id: id });
-  console.log(shopDoc);
-  res.status(200).json({
-    status: "success",
-    data: shopDoc,
-  });
-};
-
-export const getAllShops = async (req, res) => {
   try {
-    const shopDoc = await Shop.find({}).sort("-creation");
-
+    const id = req.params.id;
+    const shopDoc = await Shop.findOne({ _id: id });
     res.status(200).json({
       status: "success",
       data: shopDoc,
     });
   } catch (error) {
-    // console.log(error.message);
+    console.log(error.message);
+    return res
+      .status(424)
+      .json({ status: "Failed", message: "Request failed" });
+  }
+};
+
+export const getAllShops = async (req, res) => {
+  try {
+    const shopDoc = await Shop.find({}).sort("-creation");
+    res.status(200).json({
+      status: "success",
+      data: shopDoc,
+    });
+  } catch (error) {
+    console.log(error.message);
     return res
       .status(424)
       .json({ status: "Failed", message: "Request failed" });
