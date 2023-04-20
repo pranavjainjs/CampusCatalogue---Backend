def2fa7031d06b3302392ccea4f1d78e28d4f27d
ranjan_kuldeep <95350799+ranjankuldeep@users.noreply.github.com>
2023-04-20 22:54:36 +0530

    shop page rendered

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -3,7 +3,6 @@ import Shop from "../models/shop.model.js";
 // http://localhost:8080/api/shop/getShopById?id=643294ae0463e80940048069
 export const getShopById = async (req, res) => {
   try {
-    console.log(req);
     const id = req.query.id;
     const shopDoc = await Shop.findOne({ _id: id });
     res.status(200).json({
@@ -38,15 +37,15 @@ export const getAllShops = async (req, res) => {
 export const postShop = async (req, res, next) => {
   try {
     var { name, phone_number } = req.body;
-    console.log(name, phone_number)
+    console.log(name, phone_number);
     const doc = new Shop({
       name,
       phone_number,
-    })
+    });
     console.log(doc);
 
-   const response = await doc.save();
-   console.log(response)
+    const response = await doc.save();
+    console.log(response);
     res.json({
       status: "success",
       data: doc,
