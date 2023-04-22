04d0617cdd83fe3b9a359f73758af4be15c71ae0
Pranav <95164530+thefakepranav@users.noreply.github.com>
2023-04-22 16:17:19 +0530

    minor change

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,9 +9,9 @@ const router = Router();
 
 // post requests
 router.post("/addUser", addUser);
-router.get("/getFavouriteItems", getFavouritesItems);
 
 // get requests
+router.get("/getFavouriteItems", getFavouritesItems);
 router.get("/getUserInfo", getUserInfo);
 router.get("/getFavouriteShops", getFavouriteShops);
 
